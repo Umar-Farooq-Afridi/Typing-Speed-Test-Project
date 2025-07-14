219528ea7049d275bd53d0c10e672d6a7ad8a349
umar2144 <umerfarooq21441@gmail.com>
2025-07-14 13:37:45 +0500

    Make reset button interactive

--- script/typing-speed-test.js
+++ script/typing-speed-test.js
@@ -1,8 +1,11 @@
 import { easySentences, mediumSentences, hardSentences } from "../data/typing-speed-test-data.js";
 
-const randomSentence = getSentence();
 
-document.querySelector('.js-sentence-text-display').innerHTML = randomSentence;
+function displaySentence() {
+    document.querySelector('.js-sentence-text-display').innerHTML = getSentence();
+}
+
+displaySentence();
 
 function getSentence() {
     const difficultyLevel = document.querySelector('.js-difficulty-level').value;
@@ -23,3 +26,7 @@ function getSentence() {
 
     return sentence;
 }
+
+document.querySelector('.js-reset-button').addEventListener('click', ()=> {
+    displaySentence();
+});
