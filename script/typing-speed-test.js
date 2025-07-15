b6c12db31428ed88ab2acc54d177c0cbd7416b0a
umar2144 <umerfarooq21441@gmail.com>
2025-07-15 12:24:24 +0500

    Make the accuracy and mistakes section for the project interactive through event listener

--- script/typing-speed-test.js
+++ script/typing-speed-test.js
@@ -1,31 +1,17 @@
-import { easySentences, mediumSentences, hardSentences } from "../data/typing-speed-test-data.js";
+import { getSentence } from "../data/typing-speed-test-data.js";
 
+let sentence = getSentence();
 
 function displaySentence() {
-    document.querySelector('.js-sentence-text-display').innerHTML = getSentence();
+    sentence = getSentence();
+    document.querySelector('.js-sentence-text-display').innerHTML = sentence;
+    document.querySelector('.js-input-box').value = '';
+
+    document.querySelector('.js-text-mistakes').textContent = 0;
+    document.querySelector('.js-text-accuracy').textContent = `${100}%`;
 }
 
 displaySentence();
-
-function getSentence() {
-    const difficultyLevel = document.querySelector('.js-difficulty-level').value;
-
-    let sentence = '';
-    if (difficultyLevel === 'easy') {
-        const index = Math.floor(Math.random() * easySentences.length);
-        sentence = easySentences[index];
-    }
-    else if (difficultyLevel === 'medium') {
-        const index = Math.floor(Math.random() * mediumSentences.length);
-        sentence = mediumSentences[index];
-    }
-    else if (difficultyLevel === 'hard') {
-        const index = Math.floor(Math.random() * hardSentences.length);
-        sentence = hardSentences[index];
-    }
-
-    return sentence;
-}
 
 document.querySelector('.js-reset-button').addEventListener('click', () => {
     displaySentence();
@@ -34,4 +20,45 @@ document.querySelector('.js-reset-button').addEventListener('click', () => {
 
 document.querySelector('.js-difficulty-level').addEventListener('change', () => {
     displaySentence();
+});
+
+
+const inputField = document.querySelector('.js-input-box');
+
+inputField.addEventListener('input', () => {
+    const typed = inputField.value;
+
+    const typedWords = typed.trim().split(/\s+/); // split by ANY space
+    const sentenceWords = sentence.trim().split(/\s+/);
+
+    let correctWords = 0;
+    let mistakes = 0;
+
+    for (let i = 0; i < typedWords.length; i++) {
+        if (typedWords[i] === sentenceWords[i]) {
+            correctWords++;
+        }
+        else {
+            mistakes++;
+        }
+    }
+
+    const totalTyped = typedWords.length;
+
+    let accuracy;
+    if (totalTyped > 0) {
+        accuracy = ((correctWords / totalTyped) * 100).toFixed(2);
+    }
+    else {
+        accuracy = 100;
+    }
+
+    if (typed === '') {
+        mistakes = 0;
+        accuracy = 100;
+    }
+
+
+    document.querySelector('.js-text-mistakes').textContent = mistakes;
+    document.querySelector('.js-text-accuracy').textContent = `${accuracy}%`;
 });
